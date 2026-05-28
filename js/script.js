9b3380caaf6a1930055b2869a94757cba6b83ef1
Paulo Mello <paulofcam@gmail.com>
2026-05-28 19:42:13 -0300

    Corrigir chamada do gráfico no script.js

--- js/script.js
+++ js/script.js
@@ -63,6 +63,41 @@ function marcarAbaAtiva() {
     }
 }
 
+function desenharGrafico() {
+    var canvas = document.getElementById("grafico-inicio");
+    if (!canvas) return;
+
+    var ctx = canvas.getContext("2d");
+    var meses = ["Dez", "Jan", "Fev", "Mar", "Abr", "Mai"];
+    var valores = [870, 950, 1020, 890, 980, 1105];
+
+    var largura = canvas.width;
+    var altura = canvas.height;
+    var padH = 10;
+    var padBottom = 20;
+    var padTop = 10;
+    var maxValor = Math.max.apply(null, valores);
+    var larguraBarra = (largura - padH * 2) / meses.length;
+    var espaco = larguraBarra * 0.3;
+
+    ctx.clearRect(0, 0, largura, altura);
+
+    for (var i = 0; i < meses.length; i++) {
+        var altBarra = Math.floor(valores[i] / maxValor * (altura - padBottom - padTop));
+        var x = Math.floor(padH + i * larguraBarra + espaco / 2);
+        var y = altura - padBottom - altBarra;
+        var w = Math.floor(larguraBarra - espaco);
+
+        ctx.fillStyle = (i === meses.length - 1) ? "#2ecc71" : "#0a1f44";
+        ctx.fillRect(x, y, w, altBarra);
+
+        ctx.fillStyle = "#999999";
+        ctx.font = "9px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText(meses[i], x + w / 2, altura - 4);
+    }
+}
+
 marcarAbaAtiva();
 
 var buscaEl = document.getElementById("busca");
@@ -74,3 +109,5 @@ var contadorEl = document.getElementById("contador-sessao");
 if (contadorEl) {
     incrementarContador();
 }
+
+desenharGrafico();
